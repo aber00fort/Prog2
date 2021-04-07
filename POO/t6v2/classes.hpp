20620afc2aade5029fa5c4a6abe408f26c80be96
Perna <engcomp.lucasedu@gmail.com>
2021-04-07 17:14:43 -0300

    T6 v2 falho

---
+++ POO/t6v2/classes.hpp
@@ -0,0 +1,14 @@
+#ifndef CLASSES_HPP
+#define CLASSES_HPP
+
+class item{
+
+};
+
+class food: public item{
+
+};
+
+//class 
+
+#endif//CLASSES_HPP
---
+++ POO/t6v2/func.hpp
@@ -0,0 +1,6 @@
+#ifndef FUNC_HPP
+#define FUNC_HPP
+#include "classes.hpp"
+
+
+#endif//FUNC_HPP
